f6519e8695f550155e7d858a60328be660ceebe5
sailaharcherukuri <sailaharcherukuri@gmail.com>
2026-09-12 12:49:06 +0530

    Add If Condition

---
+++ if-condition.py
@@ -0,0 +1,20 @@
+'''
+if statement:
+-------------
+
+-->if condition become true, then it will execute inside block of code
+-->incase it become false, then it will never enter into inside block
+
+ex:
+---
+age=19
+if age>=18: #condition satisfies of enters inside if block
+    print("eligible to vote")
+print(age)
+
+age=15
+if age>=18: #condition not satisfies so not entering if block
+    print("eligible to vote")
+print(age)
+'''
+a=90,b=78 if a<b: print(a)
