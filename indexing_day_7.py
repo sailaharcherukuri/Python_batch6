d1ba9ebca518cd449f55aeef74d5e522b8bb3b43
sailaharcherukuri <sailaharcherukuri@gmail.com>
2026-09-12 12:50:39 +0530

    Add Indexing Day 7

---
+++ indexing_day_7.py
@@ -0,0 +1,15 @@
+'''
+indexing
+--------
+positive -->0
+negitive -->-1
+
+
+so=[1,2,3,4,'python']
+print(so[-1])
+
+'''
+all=[12,[1,'python',[1,4],(78,[6,7])],['java',78]]
+print(all[-2][-1][-1]) #negitive indexing searches from backside starting with index -1
+print(all[1][3][1])    # positive indexing searches from begining starting with index 0
+
