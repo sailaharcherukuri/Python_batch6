b51ea02156d21dd63fb58e141af97f6624a84e93
sailaharcherukuri <sailaharcherukuri@gmail.com>
2026-09-12 12:54:06 +0530

    Add Python Literals and Naming Rules

---
+++ literals_naming_rules in python 14-08-2026.py
@@ -0,0 +1,89 @@
+'''Tokens
+------
+
+---> Tokens are the small unit in the python..
+
+
+1. Identifier : Identifier is a name of a variable , function or class
+-------------
+
+variable:
+
+num='python'
+print(type(num))
+
+function:
+
+def add_(a,b):
+    print(a+b)
+
+add_(4,5)
+
+class
+class details:
+      pass
+peer_1=details
+
+
+2.Keywords : keywords are already saved in python for a specified reason to run.
+----------
+ex: if, else, for, why, return, print......
+
+
+3.literals: literals are the data types that need to stored in variables
+----------
+num=90
+price=789.56
+name='teja'
+
+4.operator
+---------
+
++,-,-,=,/,%
+
+
+5.statements : statements are the instructions given to the program.
+------------
+
+num=90
+age=2
+if age>=18:
+    print('age')
+
+
+6.comments: Once comments are open the lines inside will never execute in the python file.
+----------
+
+1)single line comments(#) 
+-------------------------
+
+    age=20
+    if age>=18 :#this check age is grater or equal
+       print(age)
+    
+2)multi line comments(''' ''',""".....""")
+-----------------------------------------
+used to comment more than one line
+
+
+7.Variables rules:
+-----------------
+Bad way:
+--------
+---> can't use number at 1st position eg: 2num=80
+---> special charachters anywhere eg: $num=80
+---> cannot give space eg: n um=74
+---> cannot use keywords eg: if=67, print=88
+
+Good ways:
+--------
+
+--->small and capital letters, underscore(_), numbers(except not in first position)
+
+a={'name':'teja',           --------->non meaningful name 
+   'Ac_num':'4450811858'}
+kotak_balaji_acc_details={'name':'balaji',         -------->meaningful name
+                          'Ac_num': '4450811858'}
+'''
+
+
