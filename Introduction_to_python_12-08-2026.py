9330525c65f0d08c79ffccb6c70da3ac868dc532
sailaharcherukuri <sailaharcherukuri@gmail.com>
2026-09-12 12:44:32 +0530

    Add Introduction to Python

---
+++ Introduction_to_python_12-08-2026.py
@@ -0,0 +1,55 @@
+'''object oriented programming:
+
+mainly organized on classes and objects
+
+eg: num=7
+    print(type(num))
+
+
+what is python?
+
+python is a high level language(python take cares about memory allocation), interpreter language(python executes code line-by-line, so error finding becomes easy),
+dynamically typed language (no need to mention the type of data passing to the variable)
+
+eg:(high level language)
+
+num=9
+print(type(num))
+print(id(num)) ---> stores automatically until program runs
+
+eg: (interpreter language)
+
+num=9
+num_2=90
+Print(num) ---->finds error in this line
+print(type(num))
+print(id(num))
+
+eg:(dynamically typed language)
+
+a=90
+any='python'
+nums=[1,2]
+all=(1,2)
+print(type(a)) --->int
+print(type(nums)) --->list
+print(type(any)) ---->string
+print(type(all)) ---->tuple
+
+
+why python?
+
+1)more libraries
+2)cross-platform
+3)open source
+4)simple syntax
+
+Applications:
+1)used in data science
+2)web development
+3)AI
+4)ML
+5)Deep Learning
+6)Data Analytics
+---> Python started in early 1990's , and released in the year 1991 by Guido Van Rossum and he pick the name from the series call monty python circus which is his favorite comedy show.
+---> First version of python is released in 1991 called Python 0.9.0 and current version is Python 3.14'''
